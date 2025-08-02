c1065e5dc442b092a49b4858975010732a902876
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2025-08-02 12:46:35 -0400

    add in-line error display

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,5 +6,11 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.opt.number = true
 vim.opt.relativenumber = true
-
+vim.diagnostic.config({
+  virtual_text = true,  -- shows inline error text (default: true)
+  signs = true,         -- show signs in the gutter
+  underline = true,     -- underline the error line
+  update_in_insert = false, -- don't update diagnostics while typing
+  severity_sort = true,     -- sort by severity (errors > warnings)
+})
 
