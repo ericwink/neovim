832264463c6f9be6b1a8b5dce7fd128b58f5ebb6
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2025-08-02 12:40:10 -0400

    add telescope ui for code actions

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -6,21 +6,3 @@ return {
     "neovim/nvim-lspconfig",
   },
 }
--- return {
---   "mason-org/mason-lspconfig.nvim",
---   opts = {
---     handlers = {
---       function(server_name)
---         vim.lsp.enable(server_name, {
---           on_attach = function(_, bufnr)
---             vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover Docs" })
---           end,
---         })
---       end
---     }
---   },
---   dependencies = {
---     { "mason-org/mason.nvim", opts = {} },
---   },
--- }
---
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,9 @@ return   {
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+    -- for telescope ui select on code actions
+    require("telescope").load_extension("ui-select")
   end
 }
 
