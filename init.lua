611b8bbb628355c5a74f7e05a24df19214d9d6c2
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2025-08-02 09:13:16 -0400

    add line num and relative line num

--- init.lua
+++ init.lua
@@ -4,6 +4,8 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
