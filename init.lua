da16cc60c20738be1ae96ab9edd1833a04164b07
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2025-08-02 10:15:19 -0400

    move vim options to own file

--- init.lua
+++ init.lua
@@ -1,12 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-vim.opt.number = true
-vim.opt.relativenumber = true
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -24,5 +15,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("vim-options")
 require("lazy").setup('plugins')
 
