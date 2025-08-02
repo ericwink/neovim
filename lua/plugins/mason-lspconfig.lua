e8ca54b3bfab2e0318cc7e8dd92a8b0a2177321b
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2025-08-02 12:25:35 -0400

    lsp setup and working with hover and actions

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,8 +1,26 @@
 return {
   "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-} 
+  opts = {},
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
+  },
+}
+-- return {
+--   "mason-org/mason-lspconfig.nvim",
+--   opts = {
+--     handlers = {
+--       function(server_name)
+--         vim.lsp.enable(server_name, {
+--           on_attach = function(_, bufnr)
+--             vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover Docs" })
+--           end,
+--         })
+--       end
+--     }
+--   },
+--   dependencies = {
+--     { "mason-org/mason.nvim", opts = {} },
+--   },
+-- }
+--
