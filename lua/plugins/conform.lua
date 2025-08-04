61a0531e09cf1a6f32fb6fec3ffa66464816233a
Eric <ewinkelspecht@isgvalue.com>
2025-08-04 09:44:43 -0400

    add git configs

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,22 +1,23 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      javascript = { "biome" },
-      typescript = { "biome" },
-      json = { "biome" },
-    },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-    formatters = {
-      biome = {
-        command = "biome",
-        args = { "format", "--stdin-file-path", "$FILENAME" },
-        stdin = true,
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "biome" },
+        typescript = { "biome" },
+        json = { "biome" },
       },
-    },
-  },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters = {
+        biome = {
+          command = "biome",
+          args = { "format", "--stdin-file-path", "$FILENAME" },
+          stdin = true,
+        },
+      },
+    }
+  }
 }
-
